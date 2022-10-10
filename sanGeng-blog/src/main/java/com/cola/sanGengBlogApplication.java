4db921a2be91835d407b8af74d869e67f4508356
cola99year <1620921710@qq.com>
2022-10-10 18:11:41 +0800

    easycode调好了

---
+++ sanGeng-blog/src/main/java/com/cola/sanGengBlogApplication.java
@@ -0,0 +1,15 @@
+package com.cola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: cola99year
+ * @Date: 2022/10/10 17:32
+ */
+@SpringBootApplication
+public class sanGengBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(sanGengBlogApplication.class,args);
+    }
+}
